1ece5c4ef0f5cf2319438cf941df2bc80edead47
nurdworker <nurdworker@gmail.com>
2024-11-18 01:21:06 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  // GET 요청을 보낼 함수
+  const [message, setMessage] = useState<string>("");
+
   useEffect(() => {
-    // localhost:3000/user로 GET 요청 보내기
     fetch("http://localhost:3000/user")
-      .then((response) => response.json()) // JSON 형태로 응답 받기
+      .then((response) => response.json())
       .then((data) => {
-        console.log(data); // 콘솔에 데이터 출력
+        setMessage(data.message);
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
       });
-  }, []); // 컴포넌트가 처음 렌더링될 때만 실행
+  }, []);
 
   return (
     <div className="App">
-      <div> hello Remember me project!</div>
+      <div>hello Remember me project!</div>
+      {message && <div>{message}</div>}
     </div>
   );
 }
